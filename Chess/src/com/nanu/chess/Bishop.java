545924068a1f757a0b91fc4cc3a3c9771cd93768
Avinash <avinash.madavan@gmail.com>
2013-04-14 03:16:00 -0700

    Create a preliminary version of bishop

--- Chess/src/com/nanu/chess/Bishop.java
+++ Chess/src/com/nanu/chess/Bishop.java
@@ -5,8 +5,29 @@ import java.util.ArrayList;
 public class Bishop extends Piece {
 
 	public ArrayList<Square> getLegalMoves(Board b) {
-		ArrayList<Square> moves;
-		return null;
+		ArrayList<Square> moves = new ArrayList<Square> ();
+		for ( int i = 0; i < 4; i++ )
+			getDiagonalMoves(b, moves, i);
+		return moves;
+	}
+	
+	private void getDiagonalMoves(Board b, ArrayList<Square> moves, int direction) {
+		Square temp = _square;
+		while ( true ) {
+			switch(direction) {
+				case (0): temp = b.getNE(temp); break;
+				case (1): temp = b.getNW(temp); break;
+				case (2): temp = b.getSE(temp); break;
+				case (3): temp = b.getSW(temp); break;
+			}
+			if ( temp != null ) {
+				moves.add(temp);
+				if ( temp.hasPiece() )
+					break;
+			}
+			else
+				break;
+		}
 	}
 
 }
