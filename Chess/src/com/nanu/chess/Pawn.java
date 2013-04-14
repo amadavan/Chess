1ba9a5792171f78bff9cbbeeef0a3037d4b7980a
Shrinu <beansmaister@gmail.com>
2013-04-14 05:10:02 -0700

    I think I did pawn.

--- Chess/src/com/nanu/chess/Pawn.java
+++ Chess/src/com/nanu/chess/Pawn.java
@@ -6,7 +6,26 @@ public class Pawn extends Piece {
 	
 	public ArrayList<Square> getLegalMoves(Board b) {
 		// TODO Auto-generated method stub
-		return null;
+		Square temp = _square;
+		ArrayList<Square> moves = new ArrayList<Square>();
+		if(temp.getY() == 2 && b.getN(b.getN(temp)).hasPiece() == false) {
+			moves.add(b.getN(b.getN(temp)));
+		}
+		if(b.getNE(temp).getOwner() != temp.getOwner() || b.getNW(temp).getOwner() != temp.getOwner()) {
+			moves.add(b.getNE(temp));
+			moves.add(b.getNW(temp));
+		}
+		if(b.getN(temp) != null) {
+			if(temp.getY() == 2 && !b.getN(b.getN(temp)).hasPiece() && !b.getN(temp).hasPiece()) {
+				moves.add(b.getN(b.getN(temp)));
+				moves.add(b.getN(temp));
+			}
+			else if(!b.getN(temp).hasPiece()) {
+				moves.add(b.getN(temp));
+			}
+		}	
+			
+		return moves;
 	}
 
 }
