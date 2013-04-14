c8c2628040b6cbc0233b977a7dc93733e1b921cb
Shrinu <beansmaister@gmail.com>
2013-04-14 03:15:36 -0700

    Rook legal moves function made + naming convention used.

--- Chess/src/com/nanu/chess/Rook.java
+++ Chess/src/com/nanu/chess/Rook.java
@@ -7,23 +7,23 @@ public class Rook extends Piece {
 	public ArrayList<Square> getLegalMoves(Board b) {
 		// TODO Auto-generated method stub
 		
-		ArrayList<Square> moves = new ArrayList<Square>();
-		Square temp = this._square;
-		while(b.getE(temp) != null) {
-			moves.add(temp);
-			temp = b.getE(temp);
+		ArrayList<Square> _moves = new ArrayList<Square>();
+		Square _temp = this._square;
+		while(b.getE(_temp) != null) {
+			_moves.add(_temp);
+			_temp = b.getE(_temp);
 		}
-		while(b.getW(temp) != null) {
-			moves.add(temp);
-			temp = b.getW(temp);
+		while(b.getW(_temp) != null) {
+			_moves.add(_temp);
+			_temp = b.getW(_temp);
 		}
-		while(b.getN(temp) != null) {
-			moves.add(temp);
-			temp = b.getN(temp);
+		while(b.getN(_temp) != null) {
+			_moves.add(_temp);
+			_temp = b.getN(_temp);
 		}
-		while(b.getS(temp) != null) {
-			moves.add(temp);
-			temp = b.getS(temp);
+		while(b.getS(_temp) != null) {
+			_moves.add(_temp);
+			_temp = b.getS(_temp);
 		}
 		
 		return null;
