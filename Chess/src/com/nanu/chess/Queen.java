19220f3f68d0a836e6b5199d3f9047d452484fe1
Avinash <avinash.madavan@gmail.com>
2013-04-14 03:37:14 -0700

    Added queen valid move method

--- Chess/src/com/nanu/chess/Queen.java
+++ Chess/src/com/nanu/chess/Queen.java
@@ -5,8 +5,38 @@ import java.util.ArrayList;
 public class Queen extends Piece {
 
 	public ArrayList<Square> getLegalMoves(Board b) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Square> moves = new ArrayList<Square> ();
+		for ( int i = 0; i < 8; i++ )
+			getDiagonalMoves(b, i, moves);
+		return moves;
+	}
+	
+	private void getDiagonalMoves(Board b, int direction, ArrayList<Square> moves) {
+		Square temp = _square;
+		while ( true ) {
+			switch(direction) {
+				case (0): temp = b.getN(temp); break;
+				case (1): temp = b.getNE(temp); break;
+				case (2): temp = b.getE(temp); break;
+				case (3): temp = b.getSE(temp); break;
+				case (4): temp = b.getS(temp); break;
+				case (5): temp = b.getSW(temp); break;
+				case (6): temp = b.getW(temp); break;
+				case (7): temp = b.getNW(temp); break;
+			}
+			if ( temp != null ) {
+				if ( !temp.hasPiece() )
+					moves.add(temp);
+				else if ( temp.getOwner() != _team ) {
+					moves.add(temp);
+					break;
+				}
+				else
+					break;
+			}
+			else
+				break;
+		}
 	}
 
 }
