8fa4deee27b953acb3586a25a208bd9c619c9636
Avinash <avinash.madavan@gmail.com>
2013-04-14 03:56:38 -0700

    Add knight valid move method

--- Chess/src/com/nanu/chess/Knight.java
+++ Chess/src/com/nanu/chess/Knight.java
@@ -5,8 +5,20 @@ import java.util.ArrayList;
 public class Knight extends Piece {
 
 	public ArrayList<Square> getLegalMoves(Board b) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Square> moves = new ArrayList<Square> ();
+		Square temp;
+		for ( int i = -2; i <= 2; i++ ) {
+			for ( int j = -2; j <= 2; j++ ) {
+				if ( i != 0 && j != 0 && i != j && b.isValid(j, i) ) {
+					temp = b.getSquare(j,i);
+					if ( !temp.hasPiece() )
+						moves.add(temp);
+					else if ( temp.getOwner() != _team )
+						moves.add(temp);
+				}
+			}
+		}
+		return moves;
 	}
 
 }
