ee220104ff6f09e0cb593f3c33827b4808f4720f
Avinash <avinash.madavan@gmail.com>
2013-04-14 18:24:04 -0700

    Create empty client class

---
+++ Chess/src/com/nanu/chess/client/Client.java
@@ -0,0 +1,8 @@
+package com.nanu.chess.client;
+
+public class Client {
+	// You create a socket
+	// It reads from the socket
+	// Each read is a string
+	// Parse the string and do the thing that corresponds to it
+}
