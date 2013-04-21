7e4f605c9ab1951091843999be8ca30e5a71e06f
Avinash <avinash.madavan@gmail.com>
2013-04-20 23:47:27 -0700

    Fixed pawn

--- Chess/src/com/nanu/chess/pieces/Pawn.java
+++ Chess/src/com/nanu/chess/pieces/Pawn.java
@@ -15,26 +15,26 @@ public class Pawn extends Piece {
 	public ArrayList<Square> getLegalMoves(Board b, Square s) {
 		Square temp = s;
 		ArrayList<Square> moves = new ArrayList<Square>();
-		Team team = temp.getPiece().getTeam();
-		if(team.equals(Team.WHITE) ) {
-			if(b.getN(temp) != null && b.getN(temp).getPiece() == null ) {
-				moves.add(b.getN(temp));
-				if(temp.getY() == 2 && temp.getPiece() == null) {
+//		Team team = temp.getPiece().getTeam();
+//		if(team.equals(Team.WHITE) ) {
+			if(b.getN(s) != null && b.getN(s).getPiece() == null ) {
+				moves.add(b.getN(s));
+				if(s.getY() == 6 && b.getN(b.getN(s)).getPiece() == null) {
 					moves.add(b.getN(b.getN(temp)));
 				}
 			}
 			if( b.getNE(temp) != null &&
 					b.getNE(temp).getPiece() != null &&
-					b.getNE(temp).getPiece().getTeam().equals(Team.BLACK)) {
+					!b.getNE(temp).getPiece().getTeam().equals(this.getTeam())) {
 				moves.add(b.getNE(temp));
 			}
 			if( b.getNW(temp) != null && 
 					b.getNW(temp).getPiece() != null &&
-					b.getNW(temp).getPiece().getTeam().equals(Team.BLACK)) {
+					!b.getNW(temp).getPiece().getTeam().equals(this.getTeam())) {
 				moves.add(b.getNW(temp));
 			}
-		}
-		else {
+//		}
+/*		else {
 			if(b.getS(temp) != null) {
 				moves.add(b.getS(temp));
 				if(temp.getY() == 7 && temp.getPiece() == null) {
@@ -47,7 +47,7 @@ public class Pawn extends Piece {
 			if(b.getNW(temp).getPiece().getTeam().equals(Team.WHITE)) {
 				moves.add(b.getSW(temp));
 			}
-		}
+		}*/
 		return moves;
 	}
 
